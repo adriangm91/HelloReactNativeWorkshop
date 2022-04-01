a459192d64ea819eff189d094c07321bb64eb4de
adriangm91 <adrigamo2@hotmail.com>
2022-03-31 21:12:24 -0600

    Hello ReactNative workshop

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style= {[styles.welcome, {flex: 5,backgroundColor:'pink'} ]} >Hello React Native!</Text>
+      <Text style= {[styles.welcome, {flex: 4,backgroundColor:'yellow'} ]} >Hello React Native!</Text>
+      <Text style= {[styles.welcome, {flex: 3,backgroundColor:'red'} ]} >Hello React Native!</Text>
+      <Text style= {[styles.welcome, {flex: 2,backgroundColor:'blue'} ]} >Hello React Native!</Text>
+
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around', //Esto mantiene en el centro de arriba a abajo el texto
+    //alignItems: 'center', // Esto mantiene en el centro de izq a derecha los items
+    backgroundColor:'orange'
+  },
+  welcome: {
+    fontSize: 20,
+    //textAlign: 'center',
+    margin: 10,
   },
 });
